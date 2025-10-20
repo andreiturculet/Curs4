363d55e49439185a5bd9eee5fade79126563528b
andreiturculet <andrei.turculet@emanuel.ro>
2025-10-20 13:07:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: './',  // schimbat din '' in './' pentru GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
